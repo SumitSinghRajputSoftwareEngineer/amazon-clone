c1e5887d7c2de6c778e6f6b5ac5b376150d801c7
Sumit <sumitsinghmechengineer@gmail.com>
2022-09-26 14:38:04 +0530

    added login functionality

--- src/Login.js
+++ src/Login.js
@@ -1,22 +1,30 @@
 import React, { useState } from "react";
 import "./Login.css";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { auth } from "./firebase";
 function Login() {
+  const history =  useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const signIn = (e) => {
     e.preventDefault();
-    //some fancy firebase login shitttt......
+    
+    auth.signInWithEmailAndPassword(email,password).then(auth => {
+      history.push('/')
+    }).catch(error => alert(error.message))
   };
 
   const register = (e) =>{
     e.preventDefault();
 
     auth.createUserWithEmailAndPassword(email,password).then((auth)=>{
-        console.log(auth);
-    })
+      //it successfully created a new user with email and password  
+      console.log(auth);
+      if(auth){
+        history.push('/')
+      }
+    }).catch(error => alert(error.message))
     //some fancy firebase register shitttt......
   }
   return (
